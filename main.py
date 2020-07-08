f3eea69ceb2b5fcbf7d25e53107128d3c05d3a33
czagnos <czagnos@gmail.com>
2020-07-08 20:37:26 +0300

    adding bio process

--- main.py
+++ main.py
@@ -78,13 +78,16 @@ class InstaBot:
             target_hashtags_len = len(target_hashtags)
             self.__init__(self.username,self.pw)            
             for i in range(target_hashtags_len):
+                sleep(2)  
                 target_hashtags_sharp[i] = '#' + target_hashtags_sharp[i]
                 self.driver.find_element_by_xpath("//input[@placeholder=\"Search\"]")\
                 .send_keys(target_hashtags_sharp[i])
-                sleep(4)  
+                sleep(2)  
                 self.driver.find_element_by_xpath("//a[contains(@href,'/explore/tags/{}/')]".format(target_hashtags[i]))\
                 .click()
                 sleep(5)
+                self.driver.refresh()
+
 
 
     def find_correct_hashtags(self,hashtag,country): #TODO open tagsfinder link according to country
@@ -100,6 +103,70 @@ class InstaBot:
         tempo = hashtagss[0].text.replace(' #',' ').replace('#',' ').split()
         return tempo     
 
+    def dm_selected_accounts(self,accounts,text):
+        accounts_list = accounts.split('/')
+        accounts_len = len(accounts_list)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/direct/inbox/')]")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Send Message')]")\
+            .click()
+        for i in range(accounts_len):
+            self.driver.find_element_by_xpath("//input[@placeholder=\"Search...\"]")\
+                .send_keys(accounts_list[i])
+            for j  in range(0,5):     
+                self.driver.switch_to.active_element.send_keys(Keys.TAB)
+                sleep(1)
+            self.driver.switch_to.active_element.send_keys(Keys.ENTER) 
+ 
+            self.driver.find_element_by_class_name('rIacr')\
+            .click()
+            sleep(3)
+            self.driver.find_element_by_xpath("//textarea[contains(@placeholder,'Message...')]")\
+            .send_keys(text)
+            sleep(2)  
+            self.driver.find_element_by_xpath("//button[contains(text(), 'Send')]")\
+            .click()
+            
+
+    def search_bio(self,text,min_follower):
+        self.driver.get("https://www.searchmy.bio/")
+        self.driver.find_element_by_xpath("//input[@placeholder=\"Keywords to search user bios\"]")\
+                .send_keys(text)
+        sleep(1)
+
+        
+        self.driver.find_element_by_xpath("//span[contains(text(), 'Followers:')]")\
+            .click()
+        sleep(1)
+        self.driver.find_element_by_xpath("//input[@ng-model=\"min\"]")\
+                .send_keys(min_follower)
+        sleep(1)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Apply')]")\
+            .click()    
+        sleep(1)        
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Search')]")\
+            .click()
+
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down to bottom
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            sleep(1)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        box = self.driver.find_element_by_class_name('search-results')
+        links = box.find_elements_by_class_name('search-result-username')
+        names = [name.text for name in links ]
+        return print(names)
 
     def get_unfollowers(self):
         self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
@@ -138,6 +205,8 @@ class InstaBot:
         return names
 
 my_bot = InstaBot('zagnoscan', '14447403096')
-my_bot.get_target_hashtags('art')
+my_bot.search_bio('thought leader',1000)
+#my_bot.dm_selected_accounts('isilayko/smdbcr','bu mesaj botla atılmıştır. abilere selam çatışmaya devam.')
+#my_bot.get_target_hashtags('milano')
 #my_bot.get_target_accounts_followers('smdbcr/ekinbayrakk/zagnoscan')
 #my_bot.get_unfollowers()
